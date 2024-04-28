96117b8af45e1c89941fe0a634c027dc8e7ee918
ammar yasser <aboamer0548@gmail.com>
2024-04-28 14:56:17 +0300

    New Project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,101 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useState } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min";
+let nextId = 0;
 
 function App() {
+  let [deviceInput, setDevice] = useState("");
+  let [deviceArr, setDeviceArr] = useState([]);
+  let arrDevices = deviceArr.map((item) => {
+    return (
+      <li
+        key={item.id}
+        className=" mb-3 d-flex justify-content-between align-items-center"
+      >
+        <span>
+          {" "}
+          {item.id} - {item.name}{" "}
+        </span>
+
+        <div>
+          <button
+            className="btn btn-warning me-2"
+            onClick={() => {
+              editDevice(item.id);
+            }}
+          >
+            Edit
+          </button>
+          <button
+            className="btn btn-danger "
+            onClick={() => {
+              deleteDevice(item.id);
+            }}
+          >
+            Delete
+          </button>
+        </div>
+      </li>
+    );
+  });
+
+  function addDevices() {
+    setDeviceArr([...deviceArr, { id: nextId + 1, name: deviceInput }]);
+    nextId = nextId + 1;
+    clearInput();
+  }
+  function deleteDevice(id) {
+    let newDevices = deviceArr.filter((device) => {
+      return device.id !== id;
+    });
+    nextId = nextId - 1;
+    setDeviceArr(newDevices);
+  }
+  function editDevice(id) {
+    let newDevices = deviceArr.map((device) => {
+      if (device.id === id) {
+        let newDevice = { ...device, name: prompt(device.name) };
+        
+        return newDevice;
+      } else {
+        return device;
+      }
+    });
+    setDeviceArr(newDevices);
+  }
+  function clearInput() {
+    let input = document.getElementById("input");
+    input.value = "";
+  }
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App  ">
+      <div className="py-5 bg-success text-white ">
+        <h2>To Do List</h2>
+      </div>
+      <div className="container mt-5">
+        <div className="d-flex  gap-3">
+          <input
+            id="input"
+            type="text"
+            className="form-control"
+            onChange={(e) => {
+              setDevice(e.target.value);
+            }}
+          />
+          <button
+            id="btn"
+            type="submit"
+            className="btn btn-success"
+            onClick={addDevices}
+          >
+            Add
+          </button>
+        </div>
+        <ul className="mt-3  ps-2 w-100">{arrDevices}</ul>
+      </div>
     </div>
   );
 }
